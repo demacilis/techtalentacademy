ec11135c43d4cbe6a60be2108db77366ea995721
demacilis <est.ludum@gmail.com>
2022-04-12 23:26:26 +0100

    Update tasks week 12 (SQL).sql

--- tasks week 12 (SQL).sql
+++ tasks week 12 (SQL).sql
@@ -1,9 +1,9 @@
--- Create a relational database (2 tables)
+
 CREATE DATABASE shop;
 
 USE shop;
 
--- CREATE TABLE 1 - CUSTOMER DETAILS
+
 CREATE TABLE Items( 
 ItemID INT NOT NULL,
 ItemName VARCHAR (20) NOT NULL, 
@@ -26,20 +26,20 @@ VALUES ('1', 'RAM', '2305', '10'),
 ('9', 'NETWORK CARD', '0070', '20'),
 ('10', 'CASE', '5632', '20');
 
--- Update a record
+
 SELECT * FROM Items;
 
 UPDATE Items
 SET ItemName = 'CPU'
 WHERE ItemNumber = 1205;
 
--- Delete a record
+
 SELECT * FROM Items;
 
 DELETE FROM Items 
 WHERE ItemNumber ='7';
 
--- CREATE TABLE 2 - ORDER DETAILS
+
 CREATE TABLE Customers(
 OrderNumber INT NOT NULL, 
 CustomerNumber INT NOT NULL,
@@ -59,35 +59,35 @@ VALUES ('1','1', 'Stavanger', 'Norway', '50320'),
 ('5','5', 'Reyikjavik', 'Iceland', '92310'),
 
 
--- Update a record
+
 SELECT * FROM Items;
 
 UPDATE Items
 SET Price = 40
 WHERE ItemName = 'SOUNDBOARD';
 
--- Delete a record
+
 SELECT * FROM Items;
 
 DELETE FROM Items
 WHERE ItemID ='10';
 
--- Join tables
+
 SELECT Items.ItemID, Items.ItemName, Items.Price
 FROM Items
 INNER JOIN Items ON Customers.CustomerNumber=Items.ItemID;
 
--- Run a simple query (one field/column) searching one table.
+
 SELECT * FROM Items
 WHERE Price = 20;
 
--- Run a complex query (more than one field/column) to demonstrate the relations between the 2 tables
+
 SELECT * FROM Items
 WHERE ItemName = "HDD" AND OrderPrice = 40;
 
--- Retrieve all your data sorted in ascending order on an appropriate field (one table)
+
 SELECT * FROM Customers
 ORDER BY Country;
 
--- Filter data using comparison operators (one table)
+
 SELECT OrderNumber FROM Items WHERE OrderPrice = '50';
